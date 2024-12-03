e9fd5708e2a862e54911dfb8c5463b515c0a08d0
angoloisaac83 <110981606+angoloisaac83@users.noreply.github.com>
2024-12-03 10:53:02 -0800

    Update cartaddandremove.jsx

--- src/context/cartaddandremove.jsx
+++ src/context/cartaddandremove.jsx
@@ -1,39 +1,38 @@
-const addToCart = (id,name,price)=>{
-    let data = []
-    if(JSON.parse(localStorage.getItem('cartItem')) == []){
-        data = []
-    }else{
-        data = JSON.parse(localStorage.getItem('cartItem'))
-    }
-    data.push(
-        {
-            'id': id,
-            'name': name,
-            'price': price
-        }
-    )
-    localStorage.setItem('cartItem',JSON.stringify(data))
-    console.log(data)
-    // alert("Items Added")
-}
-const emptyCart = ()=>{
-    let data = JSON.parse(localStorage.getItem('cartItem'))
-    data = []
-    localStorage.setItem('cartItem',JSON.stringify(data))
-    console.log(data)
-    alert("Cart Emptied")
-}
-const removeItemCart = (id,name,price)=>{
-    let data = JSON.parse(localStorage.getItem('cartItem'))
-    data.pop(
-        {
-            'id': id,
-            'name': name,
-            'price': price
-        }
-    )
-    localStorage.setItem('cartItem',JSON.stringify(data))
-    console.log(data)
-    alert("Items Removed")
-}
-export {addToCart, emptyCart ,removeItemCart};
+const addToCart = (id, name, price) => {
+    // Retrieve cart data from localStorage or initialize as empty array
+    let data = JSON.parse(localStorage.getItem('cartItem')) || [];
+  
+    // Add new item to the cart
+    data.push({ id, name, price });
+  
+    // Update localStorage
+    localStorage.setItem('cartItem', JSON.stringify(data));
+  
+    console.log(data);
+    alert("Item added to cart");
+  };
+  
+  const emptyCart = () => {
+    // Clear the cart by setting an empty array
+    localStorage.setItem('cartItem', JSON.stringify([]));
+  
+    console.log("Cart emptied");
+    alert("Cart Emptied");
+  };
+  
+  const removeItemCart = (id) => {
+    // Retrieve cart data
+    let data = JSON.parse(localStorage.getItem('cartItem')) || [];
+  
+    // Filter out the item to be removed
+    data = data.filter((item) => item.id !== id);
+  
+    // Update localStorage
+    localStorage.setItem('cartItem', JSON.stringify(data));
+  
+    console.log(data);
+    alert("Item removed from cart");
+  };
+  
+  export { addToCart, emptyCart, removeItemCart };
+  
